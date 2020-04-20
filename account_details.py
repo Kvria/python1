8c4d4c7c94932425e6ed5d6705c6f55a932420b1
Kvria <trishakuria@gmail.com>
2020-04-20 13:56:28 +0300

    add list function

---
+++ account_details.py
@@ -0,0 +1,22 @@
+class Details:
+    """
+    Class that generates new instances of account details.
+    """
+    details_list = [] # Empty contact list
+
+    def __init__(self, first_name, last_name, number, email,email_password,facebook_name,facebook_password,instagram_name,instagram_password,twitter_name,twitter_password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+        self.email_password = email_password
+        self.instagram_name = instagram_name
+        self.instagram_password = instagram_password
+        self.facebook_name = facebook_name
+        self.facebook_password = facebook_password
+        self.twitter_name = twitter_password
+        self.twitter_password = twitter_password
+        
+
+        
